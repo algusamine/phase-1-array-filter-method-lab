fb2b6ba2ec559d694d3f8b6e86554dc11e30db41
Amine <algusamine@gmail.com>
2021-07-06 12:12:54 -0400

    commit tree dc3e750aa0d6cef34476ff02f2efe5f817eda3ee

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+/*function findMatching(arr, name){
+    return (arr.filter(x => x === name));
+  };
+  findMatching(drivers, "Bobby");*/
+  function findMatching(arr, name){
+    return arr.filter(function(x){
+      return  name.toLowerCase() === x.toLowerCase()});
+    };
+    //findMatching(drivers, "Bobby")
+  
+    function fuzzyMatch(arr, name){
+      return arr.filter(function (x){
+        return name[0] === x[0]
+      })
+    }
+
+    function matchName(arr, name){
+      return arr.filter(function (x){
+        return x.name === name;
+      })
+    }
